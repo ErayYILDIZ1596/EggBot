61794eb266fa06ec0670726110111f34dc41c7d1
ErayYILDIZ <160312713+ErayYILDIZ1596@users.noreply.github.com>
2024-03-02 12:35:06 +0300

    Add files via upload

---
+++ EggBot.py
@@ -0,0 +1,27 @@
+import discord
+import EggBotSifre
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$merhaba'):
+        await message.channel.send("Selam!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    elif message.content.startswith("$Sifre"):
+        await message.channel.send(EggBotSifre.EggBot(12))
+    else:
+        await message.channel.send(message.content)
+
+client.run("MTIxMzQwMjYwNDIyNzI2ODYxOA.GJ-xTT.DlmG9OoXfNCYLv8lET47R4diOj64g-gykW0i3M")
---
+++ EggBotSifre.py
@@ -0,0 +1,7 @@
+import random
+
+def EggBot(length):
+    sonuc = ""
+    for i in range(length):
+        sifre = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    return sonuc
